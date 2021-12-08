1ad4ba56983bee9a17788eb954f20d457bee69d2
Büşra Şirin <sirinb18@itu.edu.tr>
2021-12-08 19:56:30 +0300

    button style

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
-import { ExampleComponent } from 'my-button-component'
+import { Button } from 'my-button-component'
 import 'my-button-component/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <>
+    <div className="btn-div">
+      <div className="">
+        <Button className="btn primary" type="primary">Primary Button</Button>
+        <Button className="btn default" >Default Button</Button>
+        <Button className="btn dashed" type="dashed">Dashed Button</Button>
+        <br />
+        <Button className="btn text" type="text">Text Button</Button>
+        <Button className="btn link" type="link">Link Button</Button>
+      </div>
+    </div>
+  </>
 }
 
 export default App
